eff7ec0565544c8fa686c9b9035ac4bfe6b75f24
Daman <34317675+damansc@users.noreply.github.com>
2020-01-06 00:37:58 -0500

    started box score scraping file

---
+++ scraping_scripts/box_score_scrape.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+import numpy as np
+import os
+
+def get_boxscore_links(year='2020'):
+
+    """
+    retrieves all links to every single available box score.
+    It seems that bball reference lags behind a bit.
+    I ran today (1/6/20) and only through 1/4/20 is up.
+    """
+    
+    months = ['october', 'november', 'december', 'january', 'february', 'march', 'april']
+    root = 'https://www.basketball-reference.com'
+    boxscore_links = []
+    for month in months:
+        source = requests.get(f'https://www.basketball-reference.com/leagues/NBA_{year}_games-{month}.html')
+        soup = BeautifulSoup(source.content, 'html.parser')
+        a_soup = soup.findAll('a')
+        a_tags = []
+
+        for a in a_soup:
+            if (a.get('href')[0:4] == '/box') & (len(a.get('href')) == 28):
+                a_tags.append(a.get('href'))
+
+        for x in a_tags:
+            boxscore_links.append(root + x)
+
+    return print(boxscore_links)
+
